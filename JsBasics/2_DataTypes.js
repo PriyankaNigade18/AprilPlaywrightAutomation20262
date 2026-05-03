0b3be25afa8a8ef742364432e73bf34752aa34d3
PriyankaNigade18 <piyu1818@gmail.com>
2026-05-03 09:11:02 +0530

    Javascript Day-3

--- JsBasics/2_DataTypes.js
+++ JsBasics/2_DataTypes.js
@@ -286,3 +286,5 @@ console.log(person);
 
 
 
+
+
--- JsBasics/3_Variables.js
+++ JsBasics/3_Variables.js
@@ -4,7 +4,7 @@ Variable
 =============
 Name of storage where we can store a data
 
-In Js we use 3 keywords to declare variable
+In Js we use 3 keywords to declare variable(var,let,const)
 1.var(not recommended /older)
 --------------------------------
 -Scope: Global +Functional
@@ -64,7 +64,120 @@ console.log("City is: "+city);
 console.log("Email is: "+email);
 
 
-//Golbal data within function
+console.log("----Global data within function----");
+
+function test1()
+{
+console.log("First name is: "+fname);
+console.log("City is: "+city);
+console.log("Email is: "+email);
+}
+
+//call
+test1();
+
+
+console.log("-------Functional scope------");
+//functional scope is applicable for var type
+function test2()
+{
+    var browserName="chrome";//local variable
+    console.log("browser name  is: "+ browserName);
+    
+    let browserVersion=147;//block scope
+    const vendorName="Google";//block scope
+    console.log("Browser version is: "+browserVersion);
+    console.log("Browser vendorName is: "+vendorName);
+    
+    
+
+}
+
+test2();
+ 
+ //console.log("Browser version is: "+browserVersion);//ReferenceError: browserVersion is not defined
+//console.log("Browser vendorName is: "+vendorName);//ReferenceError: vendorName is not defined//
+//console.log("browser name  is: "+ browserName);//ReferenceError: browserName is not defined
+
+
+console.log("------Block scope------");
+
+if(true)
+{
+    let toolVersion=1.59;
+    const toolName="Playwright";
+
+    console.log("Tool Name is: "+toolName);
+    console.log("Tool version is: "+toolVersion);
+    
+
+}
+
+//console.log("Tool Name is: "+toolName);//ReferenceError: toolName is not defined
+//console.log("Tool version is: "+toolVersion);//ReferenceError: toolVersion is not defined
+
+
+
+console.log("--------------------");
+
+var a=10;//global
+console.log(a);//10
+function test3()
+{
+ var a=20;//functional/local
+ console.log(a);//20
+ if(true)
+ {
+    var a=30;
+    console.log("if: "+a);//30
+ }
+ console.log("value of a: "+a);//30 as a is functional scope as var type
+ }
+
+test3();
+
+
+console.log("------------");
+//let and cost
+let b=10;
+console.log(b);//10
+
+function test4()
+{
+    // let b=20;
+    // console.log(b);//20
+
+    if(true)
+    {
+        let b=30;
+        console.log(b);//30
+        
+    }
+    console.log("value of b: "+b);//b is 20 as b is block scope
+    
+    
+}
+
+test4();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
